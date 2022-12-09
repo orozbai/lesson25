a47f6173dcdfe0841cd2daff056304e6a4419998
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2022-12-09 16:27:42 +0600

    created hard game version

--- hw25/src/Game.java
+++ hw25/src/Game.java
@@ -15,12 +15,46 @@ public class Game {
         System.out.println("\nВаш ход " + userMove);
         System.out.println("Ход компьютера " + computerMove);
 
-        // узнать победителя
         if (userMove == computerMove) {
             System.out.println("Ничья");
         } else if (userMove == Move.ROCK && computerMove == Move.SCISSORS ||
                 userMove == Move.PAPER && computerMove == Move.ROCK ||
                 userMove == Move.SCISSORS && computerMove == Move.PAPER) {
+            System.out.println("Ты победил");
+            userScore++;
+        } else {
+            System.out.println("Компьютер победил");
+            computerScore++;
+        }
+        boolean ans = playAgain();
+        if (ans) {
+            return gameAction();
+        } else {
+            printScoreBoard();
+        }
+        return str;
+    }
+
+    public String hardGameAction() {
+        numberOfGames++;
+        String str = "";
+        HardMove userMove = hardMovePlayer();
+        HardMove computerMove = hardMoveComputer();
+        System.out.println("\nВаш ход " + userMove);
+        System.out.println("Ход компьютера " + computerMove);
+
+        if (userMove == computerMove) {
+            System.out.println("Ничья");
+        } else if (userMove == HardMove.ROCK && computerMove == HardMove.SCISSORS ||
+                userMove == HardMove.ROCK && computerMove == HardMove.LIZARD ||
+                userMove == HardMove.PAPER && computerMove == HardMove.ROCK ||
+                userMove == HardMove.PAPER && computerMove == HardMove.CPOK ||
+                userMove == HardMove.SCISSORS && computerMove == HardMove.PAPER ||
+                userMove == HardMove.SCISSORS && computerMove == HardMove.LIZARD ||
+                userMove == HardMove.LIZARD && computerMove == HardMove.CPOK ||
+                userMove == HardMove.LIZARD && computerMove == HardMove.PAPER ||
+                userMove == HardMove.CPOK && computerMove == HardMove.SCISSORS ||
+                userMove == HardMove.CPOK && computerMove == HardMove.ROCK) {
             System.out.println("Ты победил");
             userScore++;
         } else {
@@ -76,11 +110,42 @@ public class Game {
         numberOfGames = 0;
     }
 
+    public HardMove hardMoveComputer() {
+        HardMove[] hardMoves = HardMove.values();
+        Random random = new Random();
+        int index = random.nextInt(hardMoves.length);
+        return hardMoves[index];
+    }
+
     public Move moveComputer() {
         Move[] moves = Move.values();
         Random random = new Random();
         int index = random.nextInt(moves.length);
         return moves[index];
+    }
+
+    public HardMove hardMovePlayer() {
+        String hardPlayerMove;
+        System.out.println("Введите: rock, paper, scissors, lizard, cpok, или первую букву r p s l c");
+        hardPlayerMove = scanner.nextLine();
+        hardPlayerMove = hardPlayerMove.toUpperCase();
+        char first = hardPlayerMove.charAt(0);
+        if (first == 'P' || first == 'S' || first == 'R' || first == 'L' || first == 'C') {
+            switch (first) {
+                case 'P':
+                    return HardMove.PAPER;
+                case 'S':
+                    return HardMove.SCISSORS;
+                case 'R':
+                    return HardMove.ROCK;
+                case 'L':
+                    return HardMove.LIZARD;
+                case 'C':
+                    return HardMove.CPOK;
+            }
+        }
+        System.out.println("Некоректный ввод");
+        return hardMovePlayer();
     }
 
     public Move movePlayer() {
--- hw25/src/Main.java
+++ hw25/src/Main.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        run();
+    }
+
+    public static String run() {
+        String str = "-------START GAME-------";
         Game game = new Game();
-        System.out.println("-------START GAME-------");
-        game.gameAction();
+        System.out.println("Сыграть в сложную версию? введите цифру (1-ДА 2-НЕТ)");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        if (num == 1) {
+            game.gameAction();
+        } else if (num == 2) {
+            game.hardGameAction();
+        } else {
+            System.out.println("Не та цифра");
+            return run();
+        }
+        return str;
     }
 }
